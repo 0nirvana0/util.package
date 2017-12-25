8120f1b335b2832b3fe720cf2012c615acd46622
liuqiang <liuqiang@192.168.0.113>
2017-12-25 22:20:46 +0800

    update

--- src/com/common/lang/StringUtil.java
+++ src/com/common/lang/StringUtil.java
@@ -1,9 +1,26 @@
 package com.common.lang;
 
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringUtil {
+	/**
+	 * 生成随机字符串
+	 * @param length
+	 * @return
+	 */
+	public static String getRandomString(int length) { // length表示生成字符串的长度
+		String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+		Random random = new Random();
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < length; i++) {
+			int number = random.nextInt(base.length());
+			sb.append(base.charAt(number));
+		}
+		return sb.toString();
+	}
+
 	/**
 	 * 判断字符串是否是数字
 	 */
